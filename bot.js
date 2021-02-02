c52e0d3664f0588a587e4293cc6d561616a6596c
MFerguson1449 <MFerguson1449@gmail.com>
2021-02-01 23:59:40 -0500

    added trivia answers

--- bot.js
+++ bot.js
@@ -86,6 +86,14 @@ function randomTrivia() {
   return randomQuestion;
 };
 
+// Trivia Game
+// function triviaGame {
+//   const isGameActive = true;
+//   if (isGameActive = true) {
+//     triviaGameO
+//   }
+// };
+
 // ! Trivia Object
 const trivia = {
   "name": "Trivia Questions",
@@ -93,327 +101,297 @@ const trivia = {
     {
       "id": "trivia1",
       "question": "Who is Medivh's apprentice?",
-      "answer": ""
+      "answer": "Khadgar"
     },
     {
     "id": "trivia2",
     "question": "What room in Stormwind City did you used to have to visit to qeue for a battleground?",
-    "answer": ""
+    "answer": "War Room"
   },
   {
     "id": "trivia3",
     "question": "What was the name of the Legendary that dropped only once?",
-    "answer": ""
+    "answer": "Talisan of Binding Shard"
   },
   {
       "id": "trivia4",
     "question": "What dungeon dropped the key to Shadow Labs?",
-    "answer": ""
+    "answer": "Sethekk Halls"
   },
   {
     "id": "trivia5",
     "question": "What raid leader was famous for the quote 'Many whelps, handle it!'",
-    "answer": ""
+    "answer": "Dives"
   },
   {
     "id": "trivia6",
     "question": "What was the name of the guild that is known today for their guild leader's rage on Onyxia in Vanilla WoW?",
-    "answer": ""
+    "answer": "Wipe Club"
   },
   {
     "id": "trivia7",
-    "question": "What rare world drop trinket drops anywhere within Wrath of the Lich King?",
-    "answer": ""
-  },
-  {
-    "id": "trivia8",
-    "question": "Who used to be renown for playing porn over our Ventrilo server?",
-    "answer": ""
+    "question": "What rare world drop toy(It used to be a trinket) drops anywhere within Wrath of the Lich King?",
+    "answer": "Super Simian Sphere"
   },
   {
     "id": "trivia9",
     "question": "What paladin got so upset with Unholy Dynasty that he spam shamed our guild name in trade chat?",
-    "answer": ""
+    "answer": "Vanilor"
   },
   {
     "id": "trivia10",
-    "question": "What was the minimum reputation needed to enter Naxxramas?",
-    "answer": ""
+    "question": "In order to become attuned to Naxrammas, when reputation did you need to first reach Honored with?",
+    "answer": "Argent Dawn"
   },
   {
     "id": "trivia11",
-    "question": "Who was the star of the famous YouTube video featuring the quote 'Imma chargin muh Fireball!'",
-    "answer": ""
+    "question": "What famous YouTube video did the quote 'Imma chargin muh Fireball!' come from",
+    "answer": "Jimmy: The World of Warcraft Story"
   },
   {
     "id": "trivia12",
-    "question": "Who says the famous quote 'Hello beautiful lady, you buy cat?'",
-    "answer": ""
+    "question": "What famous YouTube video did the quote 'Hello beautiful lady! You buy cat?' come from",
+    "answer": "Pepitoz: The World of Warcraft Story"
   },
   {
     "id": "trivia13",
     "question": "What YouTube channel was known for their Machinima Warcraft videos. (Ex. Crick in the Back!, ROFLMAO!, The Anti-Elf Anthem, Tank Tank Heal Tank)",
-    "answer": ""
+    "answer": "Oxhorn"
   },
   {
     "id": "trivia14",
     "question": "What YouTube channel was known for their Warcraft Music videos. (Ex. Hard Like Heroic, M.A.G.E., Ni Hao, Assassinate)",
-    "answer": ""
+    "answer": "Nyhm"
   },
   {
     "id": "trivia15",
-    "question": "How do you log out via chat?",
-    "answer": ""
+    "question": "How do you log out of World of Warcraft via chat?",
+    "answer": "/camp"
   },
   {
     "id": "trivia16",
     "question": "What was the name of the guild led by the almighty warrior 'Catscratch'?",
-    "answer": ""
+    "answer": "Eminent Gods"
   },
   {
     "id": "trivia17",
-    "question": "What was the night elf shadow priest racial ability called?",
-    "answer": ""
-  },
-  {
-    "id": "trivia18",
-    "question": "Which of the dreadlords was Arthas hell bent on killing?",
-    "answer": ""
+    "question": "What was the name of the offensive racial ability received by Night Elf Priests?",
+    "answer": "Starshards"
   },
   {
     "id": "trivia19",
     "question": "What was Deathwing's name before he became corrupted by the old gods?",
-    "answer": ""
+    "answer": "Neltharion"
   },
   {
     "id": "trivia20",
-    "question": "Which race did the shadow priest racial ability 'Devouring Plague' belong to?",
-    "answer": ""
+    "question": "Which race did the Shadow Priest racial ability 'Devouring Plague' belong to?",
+    "answer": "Undead"
   },
   {
     "id": "trivia21",
-    "question": "What part of Thousand Needles that is now underwater was once expansively barren and flat?",
-    "answer": ""
+    "question": "What part of Thousand Needles, that is now underwater, was once expansively barren and flat?",
+    "answer": "Shimmering Flats"
   },
   {
     "id": "trivia22",
     "question": "Which weapon would initiate an event if worn while within The Scarlet Monastary?",
-    "answer": ""
+    "answer": "Corrupted Ashbringer"
   },
   {
     "id": "trivia23",
-    "question": "What was the name of the item that if used would kill all enemies within 30 yards of you? * * Hint * * : It was used as an exploit in Ulduar.",
-    "answer": ""
+    "question": "What was the name of the item that if used would kill all enemies within a 30 yard radius of you? * * Hint * * : It was used as an exploit in Ulduar.",
+    "answer": "Martin Fury"
   },
   {
     "id": "trivia24",
-    "question": "The use of what abilitity/item led to the banning of a guild upon their World First Heroic Lich King kill?",
-    "answer": ""
+    "question": "The use of what engineering item led to the banning of a guild upon their World First Heroic Lich King kill?",
+    "answer": "Saronite Bombs"
   },
   {
     "id": "trivia25",
-    "question": "What item was used to open a locked door within Ulduman?",
-    "answer": ""
+    "question": "What boss emerges from a hidden door, that is opened with the Staff of Prehistoria?",
+    "answer": "Ironaya"
   },
   {
     "id": "trivia26",
     "question": "Which encounter was was said to be statistically impossible during the Burning Crusade, until fixed in a later patch?",
-    "answer": ""
+    "answer": "M'uru"
   },
   {
     "id": "trivia27",
     "question": "During the Warlords of Draenor expansion, what was the name of the zone was set to be released but never was?",
-    "answer": ""
+    "answer": "Zangermarsh"
   },
   {
     "id": "trivia28",
-    "question": "Which melee DPS trinket that dropped in Icecrown Citadel would morph your appearance ?",
-    "answer": ""
-  },
-  {
-    "id": "trivia29",
-    "question": "Which class in Wrath of the Lich King was fabled to increase their DPS by standing in fire?",
-    "answer": ""
-  },
-  {
-    "id": "trivia30",
-    "question": "Which Unholy Dynasty member enjoying using 'brutal' as an adjective?",
-    "answer": ""
+    "question": "Which melee DPS trinket that dropped in Icecrown Citadel would morph your appearance?",
+    "answer": "Deathbringer's Will"
   },
   {
     "id": "trivia31",
     "question": "What is the name of the shadow priest artifact weapon?",
-    "answer": ""
+    "answer": "Xal,atath, Blade of the Black Empire"
   },
   {
     "id": "trivia32",
     "question": "What was Varian Wrynn's gladiator name?",
-    "answer": ""
+    "answer": "Lo'Gosh"
   },
   {
     "id": "trivia33",
     "question": "Who is the Loa of Kings?",
-    "answer": ""
+    "answer": "Rezan"
   },
   {
     "id": "trivia34",
-    "question": "In Classic, what level would a priest learn Shadowform?",
-    "answer": ""
-  },
-  {
-    "id": "trivia35",
-    "question": "When did Unholy Dynasty get the first Festergut kill? (month/day/year) :^)",
-    "answer": ""
+    "question": "In Classic, what level could a priest learn Shadowform?",
+    "answer": "40"
   },
   {
     "id": "trivia36",
     "question": "How many Dragon Aspects are there?",
-    "answer": ""
+    "answer": "5"
   },
   {
     "id": "trivia37",
     "question": "Where is Sentinel Hill?",
-    "answer": ""
+    "answer": "Westfall"
   },
   {
     "id": "trivia38",
-    "question": "Which patch added Black Temple?",
-    "answer": ""
+    "question": "What patch added Black Temple?",
+    "answer": "2.1"
   },
   {
     "id": "trivia39",
     "question": "What is the name of the Alliance specific Pandaren Faction?",
-    "answer": ""
+    "answer": "Tushui"
   },
   {
     "id": "trivia40",
-    "question": "What did spirit originally do?",
-    "answer": ""
-  },
-  {
-    "id": "trivia41",
-    "question": "What was the minimum reputation needed to get into Naxxramas in Classic?",
-    "answer": ""
+    "question": "What secondary stat did blizzard remove in patch 7.0.3 that used to increased your mana regeneration?",
+    "answer": "Spirit"
   },
   {
     "id": "trivia42",
-    "question": "What was the name of the special event boss in BC Karazhan",
-    "answer": ""
+    "question": "What was the name of the summonable boss in Burning Crusade's Karazhan",
+    "answer": "Nightbane"
   },
   {
     "id": "trivia43",
     "question": "Where did Millhouse Manastorm first appear?",
-    "answer": ""
+    "answer": "Arcatraz"
   },
   {
     "id": "trivia44",
     "question": "Who created the Emerald Dream?",
-    "answer": ""
+    "answer": "Freya"
   },
   {
     "id": "trivia45",
     "question": "What was Varian Wrynn's wifes name?",
-    "answer": ""
+    "answer": "Tiffin"
   },
   {
     "id": "trivia46",
     "question": "What is the name of the island that is home to the Night Elves World Tree?",
-    "answer": ""
+    "answer": "Teldrassil"
   },
   {
     "id": "trivia47",
     "question": "Who is the female Maghar Orc mate of Thrall?",
-    "answer": ""
+    "answer": "Aggra"
   },
   {
     "id": "trivia48",
     "question": "The two-headed wolf, Omen, can be fought during which world event?",
-    "answer": ""
+    "answer": "Lunar Festival"
   },
   {
     "id": "trivia49",
     "question": "What was the first type of bandage learned with the First Aid proffession?",
-    "answer": ""
+    "answer": "Linen Bandage"
   },
   {
     "id": "trivia50",
     "question": "What scepter could only be acquired by one player per server and was needed to open the gates of Ahn\'Qiraj?",
-    "answer": ""
+    "answer": "The Scepter of the Shifting Sands"
   },
   {
     "id": "trivia51",
     "question": "Who was Jaina Proudmoore\'s mentor in the Kirin Tor?",
-    "answer": ""
+    "answer": "Antonidas"
   },
   {
     "id": "trivia52",
     "question": "In which zone can the abomination known as Stitches be found?",
-    "answer": ""
+    "answer": "Duskwood"
   },
   {
     "id": "trivia53",
     "question": "Which Dragon Aspect leads the bronze dragonflight?",
-    "answer": ""
+    "answer": "Nozdormu"
   },
   {
     "id": "trivia54",
-    "question": "Who was the elemental lord of air?",
-    "answer": ""
+    "question": "Who was the original elemental lord of air?",
+    "answer": "Al'Akir"
   },
   {
     "id": "trivia55",
     "question": "Garona Halforcen killed which Human king?",
-    "answer": ""
+    "answer": "Llane Wrynn"
   },
   {
     "id": "trivia56",
     "question": "Don Carlos Famous Hat allows owners to summon what type of incorporeal spirit?",
-    "answer": ""
+    "answer": "Coyote"
   },
   {
     "id": "trivia57",
     "question": "Which ogre was once an apprentice of Gul\'dan and would later lead the Twilight Hammer?",
-    "answer": ""
+    "answer": "Cho'Gall"
   },
   {
     "id": "trivia58",
     "question": "Dalaran was originally located in which Eastern Kingdoms zone?",
-    "answer": ""
+    "answer": "Hillsbrad Foothills"
   },
   {
     "id": "trivia59",
     "question": "What Dark Iron Dwarf boss can be fought in Blackrock Depths during Brewfest?",
-    "answer": ""
+    "answer": "Coren Direbrew"
   },
   {
     "id": "trivia60",
     "question": "Which Tauren leader was killed by Garrosh Hellscream?",
-    "answer": ""
+    "answer": "Cairne Bloodhoof"
   },
   {
     "id": "trivia61",
     "question": "In which zone can the Gallywix Pleasure Palace be found?",
-    "answer": ""
+    "answer": "Azshara"
   },
   {
     "id": "trivia62",
     "question": "Who was the last Pandaren emperor of Pandaria?",
-    "answer": ""
+    "answer": "Shaohao"
   },
   {
     "id": "trivia63",
-    "question": "Nightsong Battlefear is a raid tier set for which class?",
-    "answer": ""
+    "question": "Nightsong Battlegear is a raid tier set for which class?",
+    "answer": "Druid"
   },
   {
     "id": "trivia64",
     "question": "What zone did Goblins reshape to more resemble the symbol of the horde?",
-    "answer": ""
+    "answer": "Azshara"
   },
   {
     "id": "trivia65",
     "question": "What was the name of the former leader of the Kirin Tor and husband Vereesa Windrunner?",
-    "answer": ""
+    "answer": "Rhonin"
   },
   ]
 };
